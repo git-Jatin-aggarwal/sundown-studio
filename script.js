a6340b0e4d689630ecbe9340dfff18a9c13d0c7c
Jatin Aggarwal <jaggarwal147@gmail.com>
2024-06-06 16:58:33 +0530

    day3

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var fixed = document.querySelector("#image-fixed")
+var elemC = document.querySelector("#elem-container")
+
+elemC.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
